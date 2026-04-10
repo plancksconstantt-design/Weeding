e70ce9fed693e8c0760413695abd407e339289e7
anna <plancksconstantt@gmail.com>
2026-04-10 14:45:19 +0300

    add presents

--- js/roulette.js
+++ js/roulette.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
-    $('.btn-send').click(function() {
-        // Генерируем случайное число от 1 до 3
-        const randomNum = Math.floor(Math.random() * 3) + 1; 
+   
+      //кнопка узнать подарок
+        $('.btn-send').click(function(){
+    let images = [
+        'img/presents/present_1.png',
+        'img/presents/present_2.png',
+        'img/presents/present_3.png',
+        'img/presents/present_4.png'
+    ];
+    
+    let randomIndex = Math.floor(Math.random() * images.length);
+    $('.present').attr('src', images[randomIndex]);
+
+     $('.presentCard-div').append('<button class="yes-button text-CormorantSemiBold">Выбрать</button>');
     });
+
+
+
+
+
+
+
 
      // КНОПКИ
     $('.whiteButton')[0].addEventListener(
